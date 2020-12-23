59da13e78171722b27f355408ab81b6b8798443a
Rostislav Svoboda <rsvoboda@redhat.com>
2020-12-23 23:58:33 +0100

    Update of SpringUnitTest

--- spring-di/src/test/java/com/baeldung/di/spring/SpringUnitTest.java
+++ spring-di/src/test/java/com/baeldung/di/spring/SpringUnitTest.java
@@ -1,55 +1,50 @@
 package com.baeldung.di.spring;
 
-import static org.junit.Assert.assertNotNull;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import javax.enterprise.inject.spi.CDI;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { SpringMainConfig.class })
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
 public class SpringUnitTest {
-	@Autowired
-	ApplicationContext context;
 
 	@Test
 	public void givenAccountServiceAutowiredToUserService_WhenGetAccountServiceInvoked_ThenReturnValueIsNotNull() {
-		UserService userService = context.getBean(UserService.class);
+		UserService userService = CDI.current().select(UserService.class).get();
 		assertNotNull(userService.getAccountService());
 	}
 
 	@Test
 	public void givenBookServiceIsRegisteredAsBeanInContext_WhenBookServiceIsRetrievedFromContext_ThenReturnValueIsNotNull() {
-		BookService bookService = context.getBean(BookService.class);
+		BookService bookService = CDI.current().select(BookService.class).get();
 		assertNotNull(bookService);
 	}
 
 	@Test
 	public void givenBookServiceIsRegisteredAsBeanInContextByOverridingAudioBookService_WhenAudioBookServiceIsRetrievedFromContext_ThenNoSuchBeanDefinitionExceptionIsThrown() {
-		BookService bookService = context.getBean(BookService.class);
+		BookService bookService = CDI.current().select(BookService.class).get();
 		assertNotNull(bookService);
-		AudioBookService audioBookService = context.getBean(AudioBookService.class);
+		AudioBookService audioBookService = CDI.current().select(AudioBookService.class).get();
 		assertNotNull(audioBookService);
 	}
 
 	@Test
 	public void givenAuthorServiceAutowiredToBookServiceAsOptionalDependency_WhenBookServiceIsRetrievedFromContext_ThenNoSuchBeanDefinitionExceptionIsNotThrown() {
-		BookService bookService = context.getBean(BookService.class);
+		BookService bookService = CDI.current().select(BookService.class).get();
 		assertNotNull(bookService);
 	}
 
 	@Test
 	public void givenSpringPersonServiceConstructorAnnotatedByAutowired_WhenSpringPersonServiceIsRetrievedFromContext_ThenInstanceWillBeCreatedFromTheConstructor() {
-		SpringPersonService personService = context.getBean(SpringPersonService.class);
+		SpringPersonService personService = CDI.current().select(SpringPersonService.class).get();
 		assertNotNull(personService);
 	}
 
 	@Test
 	public void givenPersonDaoAutowiredToSpringPersonServiceBySetterInjection_WhenSpringPersonServiceRetrievedFromContext_ThenPersonDaoInitializedByTheSetter() {
-		SpringPersonService personService = context.getBean(SpringPersonService.class);
+		SpringPersonService personService = CDI.current().select(SpringPersonService.class).get();
 		assertNotNull(personService);
 		assertNotNull(personService.getPersonDao());
 	}
