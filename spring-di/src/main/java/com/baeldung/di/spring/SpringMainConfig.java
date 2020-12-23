68c9f76fdcd181b4dc913977f564f09705784beb
Rostislav Svoboda <rsvoboda@redhat.com>
2020-12-23 23:22:26 +0100

    Use Quarkus and make the app compile (skipping tests compilation)

--- spring-di/src/main/java/com/baeldung/di/spring/SpringMainConfig.java
+++ spring-di/src/main/java/com/baeldung/di/spring/SpringMainConfig.java
@@ -1,13 +1,13 @@
 package com.baeldung.di.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+//import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SpringBeansConfig.class })
-@ComponentScan("com.baeldung.di.spring")
+//@Import({ SpringBeansConfig.class })
+//@ComponentScan("com.baeldung.di.spring")
 public class SpringMainConfig {
 
 	@Bean
